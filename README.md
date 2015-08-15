b94639090a5586fcd8ee7bf373407c3f83736c14
Ashley Rudland <ashleyr@circularwave.co.uk>
2015-08-15 22:49:33 +0100

    screentime for AngularJS forked

--- README.md
+++ README.md
@@ -1,22 +1,20 @@
 Screentime
 ==========
 
-[Screentime](http://screentime.parsnip.io/) is a small tool that helps you start thinking of your website traffic in terms of time instead of hits (Pageviews, visits, etc). You can define areas of the page, called Fields, and then Screentime will keep track of how much time each Field is on screen for. You can also use it to track smaller elements, like ad units.
+[Screentime] is a small tool that helps you start thinking of your website traffic in terms of time instead of hits (Pageviews, visits, etc). You can define areas of the page, called Fields, and then Screentime will keep track of how much time each Field is on screen for. You can also use it to track smaller elements, like ad units.
 
-Screentime only handles the client side work. You'll need to provide your own backend to post the data to. I've included an example that shows how to this with [Keen IO](https://keen.io/) using only a few lines of code. There's also a built-in option for posting to Google Analytics but there are some caveats (see below).
-
-[**View the Project Page**](http://screentime.parsnip.io/)
+Screentime only handles the client side work. You'll need to provide your own backend to post the data to.
 
 ## How it works
-You specify some DOM elements that you want to track and then every second Screentime checks the viewport to see which ones are in view. It tracks the viewable seconds for each element/field and then issues a report every 10 seconds (you can adjust the interval). The report is passed to a callback function that you can use to post the data to your server.
+You specify some DOM elements that you want to track and then every second Screentime checks the viewport to see which ones are in view. It tracks the viewable seconds for each element/field and then issues a report every 2 seconds (you can adjust the interval). The report is passed to a callback function that you can use to post the data to your server.
 
 If the user switches tabs, the timer stops (using Addy Osmani's [Page Visibility polyfill](https://github.com/addyosmani/visibly.js)). The timer doesn't require that the user be active, just that the screen elements are visible.
 
 ## Usage
-jQuery is required. Pass in selectors for each **unique element** you want to track, including a name for each. The callback option receives an object containing the data.
+jQuery is NOT required in this version. Pass in the selector for each **unique element** you want to track, including a name for each. The callback option receives an object containing the data.
 
 ```javascript
-$.screentime({
+$screentime({
   fields: [
     { selector: '#top',
       name: 'Top'
@@ -37,35 +35,17 @@ $.screentime({
 
 ## Options
 #### `fields` array (required)
-An array of object listing the DOM elements you want to track. Each object should specify a `selector` property and a `name` property.
+An array of object listing the DOM elements you want to track. Each object should specify the `selector` property and a `name` property.
 
 #### `reportInterval` number
-The interval, in seconds, used to issue a report. The default is 10 seconds.
+The interval, in seconds, used to issue a report. The default is 2 seconds.
 
 #### `percentOnScreen` string
 This determines what percentage of the field must be on screen for it to be considered in view. The default is `50%`. One exception to this rule: if a field occupies the majority of the viewport it will be considered in view regardless of its viewable percentage.
-
-#### `googleAnalytics` boolean
-Setting this to true (default is false) will send a Google Analytics event for each field (if the field has data to report) when the report is issued.
 
 #### `callback` function
 The callback function that receives the screentime data.
 
 ## Methods
 #### `reset`
-Calling `$.screentime.reset()` will reset the counter. This is useful for Single Page Applications where the page context is reloaded client side.
-
-## Posting the data
-The built-in Google Analytics option is an easy way to quickly start collecting screentime data, but it's not exactly scalable. For each field that has data to report, a separate GA Event has to be sent. If you're only tracking 2 or 3 fields, this is probably fine. But anything more than that and you might start hitting the [GA collection limit](https://developers.google.com/analytics/devguides/collection/gajs/limits-quotas).
-
-For scalabla data collection you'll want to provide your own backend or use a service like Keen IO. In the examples folder there's a demo showing how easy it is to use Keen for this. It's as simple as this:
-
-```javascript
-...
-callback: function(data) {
-  Keen.addEvent("screentime", data);        
-}
-...
-```
-
-
+Calling `$screentime.reset()` will reset the counter.
--- screentime.js
+++ screentime.js
@@ -1,72 +1,47 @@
 /*!
- * @preserve
+ * visibly - v0.6 Aug 2011 - Page Visibility API Polyfill
+ * http://github.com/addyosmani
+ * Copyright (c) 2011 Addy Osmani
+ * Dual licensed under the MIT and GPL licenses.
+ *
+ * Methods supported:
+ * visibly.onVisible(callback)
+ * visibly.onHidden(callback)
+ * visibly.hidden()
+ * visibly.visibilityState()
+ * visibly.visibilitychange(callback(state));
+ */
+
+(function(){window.visibly={q:document,p:undefined,prefixes:["webkit","ms","o","moz","khtml"],props:["VisibilityState","visibilitychange","Hidden"],m:["focus","blur"],visibleCallbacks:[],hiddenCallbacks:[],genericCallbacks:[],_callbacks:[],cachedPrefix:"",fn:null,onVisible:function(i){if(typeof i=="function"){this.visibleCallbacks.push(i)}},onHidden:function(i){if(typeof i=="function"){this.hiddenCallbacks.push(i)}},getPrefix:function(){if(!this.cachedPrefix){for(var i=0;b=this.prefixes[i++];){if(b+this.props[2]in this.q){this.cachedPrefix=b;return this.cachedPrefix}}}},visibilityState:function(){return this._getProp(0)},hidden:function(){return this._getProp(2)},visibilitychange:function(i){if(typeof i=="function"){this.genericCallbacks.push(i)}var t=this.genericCallbacks.length;if(t){if(this.cachedPrefix){while(t--){this.genericCallbacks[t].call(this,this.visibilityState())}}else{while(t--){this.genericCallbacks[t].call(this,arguments[0])}}}},isSupported:function(i){return this.cachedPrefix+this.props[2]in this.q},_getProp:function(i){return this.q[this.cachedPrefix+this.props[i]]},_execute:function(i){if(i){this._callbacks=i==1?this.visibleCallbacks:this.hiddenCallbacks;var t=this._callbacks.length;while(t--){this._callbacks[t]()}}},_visible:function(){window.visibly._execute(1);window.visibly.visibilitychange.call(window.visibly,"visible")},_hidden:function(){window.visibly._execute(2);window.visibly.visibilitychange.call(window.visibly,"hidden")},_nativeSwitch:function(){this[this._getProp(2)?"_hidden":"_visible"]()},_listen:function(){try{if(!this.isSupported()){if(this.q.addEventListener){window.addEventListener(this.m[0],this._visible,1);window.addEventListener(this.m[1],this._hidden,1)}else{if(this.q.attachEvent){this.q.attachEvent("onfocusin",this._visible);this.q.attachEvent("onfocusout",this._hidden)}}}else{this.q.addEventListener(this.cachedPrefix+this.props[1],function(){window.visibly._nativeSwitch.apply(window.visibly,arguments)},1)}}catch(i){}},init:function(){this.getPrefix();this._listen()}};this.visibly.init()})();
+
+/*!
  * Screentime.js | v0.2
  * Copyright (c) 2015 Rob Flaherty (@robflaherty)
  * Licensed under the MIT and GPL licenses.
  */
 
-(function($, window, document) {
+angular.module('screentime', []);
 
-  var defaults = {
-    fields: [],
-    percentOnScreen: '50%',
-    reportInterval: 10,
-    googleAnalytics: false,
-    callback: function(){}
-  };
+angular.module('screentime')
 
-  $.screentime = function(options) {
-    options = $.extend({}, defaults, options);
+.factory('$screentime',
 
-    // Convert perecent string to number
-    options.percentOnScreen = parseInt(options.percentOnScreen.replace('%', ''), 10);
+  function($window) {
+
+    var defaults = {
+      fields: [],
+      percentOnScreen: '50%',
+      reportInterval: 1,
+      callback: function(){}
+    };
 
     var counter = {};
     var cache = {};
     var log = {};
     var looker = null;
     var started = false;
-    var universalGA, classicGA;
-
-    if (!options.fields.length) {
-      return;
-    }
-
-    if (options.googleAnalytics) {
-
-      if (typeof ga === "function") {
-        universalGA = true;
-      }
-
-      if (typeof _gaq !== "undefined" && typeof _gaq.push === "function") {
-        classicGA = true;
-      }
-
-    }
-
-    /*
-     * Utilities
-     */
-
-    /*!
-     * visibly - v0.6 Aug 2011 - Page Visibility API Polyfill
-     * http://github.com/addyosmani
-     * Copyright (c) 2011 Addy Osmani
-     * Dual licensed under the MIT and GPL licenses.
-     *
-     * Methods supported:
-     * visibly.onVisible(callback)
-     * visibly.onHidden(callback)
-     * visibly.hidden()
-     * visibly.visibilityState()
-     * visibly.visibilitychange(callback(state));
-     */
-
-    (function(){window.visibly={q:document,p:undefined,prefixes:["webkit","ms","o","moz","khtml"],props:["VisibilityState","visibilitychange","Hidden"],m:["focus","blur"],visibleCallbacks:[],hiddenCallbacks:[],genericCallbacks:[],_callbacks:[],cachedPrefix:"",fn:null,onVisible:function(i){if(typeof i=="function"){this.visibleCallbacks.push(i)}},onHidden:function(i){if(typeof i=="function"){this.hiddenCallbacks.push(i)}},getPrefix:function(){if(!this.cachedPrefix){for(var i=0;b=this.prefixes[i++];){if(b+this.props[2]in this.q){this.cachedPrefix=b;return this.cachedPrefix}}}},visibilityState:function(){return this._getProp(0)},hidden:function(){return this._getProp(2)},visibilitychange:function(i){if(typeof i=="function"){this.genericCallbacks.push(i)}var t=this.genericCallbacks.length;if(t){if(this.cachedPrefix){while(t--){this.genericCallbacks[t].call(this,this.visibilityState())}}else{while(t--){this.genericCallbacks[t].call(this,arguments[0])}}}},isSupported:function(i){return this.cachedPrefix+this.props[2]in this.q},_getProp:function(i){return this.q[this.cachedPrefix+this.props[i]]},_execute:function(i){if(i){this._callbacks=i==1?this.visibleCallbacks:this.hiddenCallbacks;var t=this._callbacks.length;while(t--){this._callbacks[t]()}}},_visible:function(){window.visibly._execute(1);window.visibly.visibilitychange.call(window.visibly,"visible")},_hidden:function(){window.visibly._execute(2);window.visibly.visibilitychange.call(window.visibly,"hidden")},_nativeSwitch:function(){this[this._getProp(2)?"_hidden":"_visible"]()},_listen:function(){try{if(!this.isSupported()){if(this.q.addEventListener){window.addEventListener(this.m[0],this._visible,1);window.addEventListener(this.m[1],this._hidden,1)}else{if(this.q.attachEvent){this.q.attachEvent("onfocusin",this._visible);this.q.attachEvent("onfocusout",this._hidden)}}}else{this.q.addEventListener(this.cachedPrefix+this.props[1],function(){window.visibly._nativeSwitch.apply(window.visibly,arguments)},1)}}catch(i){}},init:function(){this.getPrefix();this._listen()}};this.visibly.init()})();
-
-    function random() {
-      return Math.round(Math.random() * 2147483647);
-    }
+    var options = defaults;
+    var reporter = null;
 
     /*
      * Constructors
@@ -74,38 +49,19 @@
 
     function Field(elem) {
       this.selector = elem.selector;
-      $elem = this.$elem = $(elem.selector);
+      this.$elem = document.body.querySelector(elem.selector);
       this.name = elem.name;
-
-      this.top = $elem.offset().top;
-      this.height = $elem.height();
+      this.top = this.$elem.getBoundingClientRect().top;
+      this.height = this.$elem.offsetHeight;
       this.bottom = this.top + this.height;
-      this.width = $elem.width();
+      this.width = this.$elem.offsetWidth;
     }
 
     function Viewport() {
-      var $window = $(window);
-
-      this.top = $window.scrollTop();
-      this.height = $window.height();
+      this.top = $window.pageYOffset;
+      this.height = $window.innerHeight;
       this.bottom = this.top + this.height;
-      this.width = $window.width();
-    }
-
-    /*
-     * Do Stuff
-     */
-
-    function sendGAEvent(field, time) {
-
-      if (universalGA) {
-        ga('send', 'event', 'Screentime', 'Time on Screen', field, parseInt(time, 10), {'nonInteraction': true});
-      }
-
-      if (classicGA) {
-        _gaq.push(['_trackEvent', 'Screentime', 'Time on Screen', field, parseInt(time, 10), true]);
-      }
-
+      this.width = $window.innerWidth;
     }
 
     function onScreen(viewport, field) {
@@ -116,7 +72,7 @@
         return true;
       }
 
-       // Field bigger than viewport
+      // Field bigger than viewport
       if (field.height > viewport.height) {
 
         cond = (viewport.bottom - field.top) > (viewport.height / 2) && (field.bottom - viewport.top) > (viewport.height / 2);
@@ -132,41 +88,34 @@
       partialView = ((viewport.bottom - buffered) >= field.top && (field.bottom - buffered) > viewport.top);
 
       return partialView;
-
     }
 
     function checkViewport() {
       var viewport = new Viewport();
 
-      $.each(cache, function(key, val) {
+      for (var key in cache) {
+        var val = cache[key];
         if (onScreen(viewport, val)) {
           log[key] += 1;
           counter[key] += 1;
         }
-      });
-
+      };
     }
 
     function report() {
-
       var data = {};
 
-      $.each(counter, function(key, val) {
+      for (var key in counter) {
+        var val = counter[key];
         if (val > 0) {
           data[key] = val;
           counter[key] = 0;
-
-          if (options.googleAnalytics) {
-            sendGAEvent(key, val);
-          }
-
         }
-      });
+      };
 
-      if (!$.isEmptyObject(data)) {
+      if (Object.keys(data).length > 0) {
         options.callback.call(this, data, log);
       }
-
     }
 
     function startTimers() {
@@ -191,22 +140,31 @@
       clearInterval(reporter);
     }
 
-    $.screentime.reset = function() {
+    var reset = function() {
       stopTimers();
 
-      $.each(cache, function(key, val) {
+      cache.forEach(function(val, key) {
         log[key] = 0;
         counter[key] = 0;
       });
 
       startTimers();
-    }
+    };
 
-    function init() {
+    var start = function(newOptions) {
 
-      $.each(options.fields, function(index, elem) {
-        if ($(elem.selector).length) {
-          var field = new Field(elem);
+      options = angular.extend({}, defaults, newOptions);
+
+      // Convert percent string to number
+      options.percentOnScreen = parseInt(options.percentOnScreen.replace('%', ''), 10);
+
+      if (!options.fields.length) {
+        return;
+      }
+
+      options.fields.forEach(function(fieldOption) {
+        if (document.body.querySelector(fieldOption.selector) !== null) {
+          var field = new Field(fieldOption);
           cache[field.name] = field;
           counter[field.name] = 0;
           log[field.name] = 0;
@@ -223,11 +181,11 @@
         stopTimers();
         startTimers();
       });
+    };
 
-    }
+    return {
+      start: start,
+      reset: reset
+    };
 
-    init();
-
-  };
-
-})(jQuery, window, document);
+  });
